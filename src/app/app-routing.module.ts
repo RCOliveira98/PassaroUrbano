dfaf30559b679d0997d6e333246d71c7fd6cdbd3
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-05-30 12:31:29 -0300

    Initial configuration of global routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+/*
+ * english: components to be mapped to the global router.
+ * português: componentes a serem mapeados para o roteador global.
+ */
+import { HomeComponent } from './home/home.component';
+import { FunComponent } from './fun/fun.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'fun', component: FunComponent},
+  {path: 'restaurant', component: RestaurantComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

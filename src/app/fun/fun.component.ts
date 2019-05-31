c011a578d65539b4fe8ef132938f6bad7e52802e
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-05-31 15:49:24 -0300

    fun component: Displaying data dynamically through a fake api.

--- src/app/fun/fun.component.ts
+++ src/app/fun/fun.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OfertaModel } from '../shared/oferta.model';
+import { OfertasService } from '../ofertas.service';
+
+
 @Component({
   selector: 'app-fun',
   templateUrl: './fun.component.html',
-  styleUrls: ['./fun.component.css']
+  styleUrls: ['./fun.component.css'],
+  providers: [OfertasService]
 })
 export class FunComponent implements OnInit {
 
-  constructor() { }
+  constructor(private servFun: OfertasService) { }
+
+  public myFun: OfertaModel[];
 
   ngOnInit() {
+    this.getFunAll();
   }
 
+  private getFunAll(): void {
+    this.servFun.getOffersByCategory('diversao')
+    .then((responsePromise: OfertaModel[]) => this.myFun = responsePromise)
+    .catch((responsePromise: any) => alert(responsePromise));
+  }
 }

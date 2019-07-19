fd6ceae69349cffb6fd820e920447997f34e2a89
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-07-18 21:28:34 -0300

    Capturando string de busca e acionando método de search.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,28 @@
+import { OfertaModel } from './../shared/oferta.model';
+import { Observable } from 'rxjs';
+import { OfertasService } from './../ofertas.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [OfertasService]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ofertaService: OfertasService) { }
 
+  public ofertas: Observable<OfertaModel[]>;
   ngOnInit() {
+  }
+
+  mySearchOffer(search: string): void {
+    this.ofertas = this.ofertaService.getSearchOffers(search);
+    this.ofertas.subscribe(
+      (success: OfertaModel[]) => console.log(success),
+      (err: any) => console.log(err)
+    );
   }
 
 }
